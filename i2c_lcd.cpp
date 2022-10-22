3cd7c5ffaa0aa081bb5d861c1c4782fe06147117
Suleiman Umudov <suleiman.umudov.triare@gmail.com>
2022-10-22 16:40:25 +0300

    i2c lcd 1602 2004

---
+++ i2c_lcd.cpp
@@ -0,0 +1,174 @@
+#include "i2c_lcd.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+I2C_LCD::I2C_LCD(uint8_t address, uint8_t cols, uint8_t rows) {
+
+	this->lcd_function = LCD_4BITMODE | LCD_5x8DOTS;
+	this->address = (address << 1); // shift address by one bit on stm32
+	this->cols = cols;
+	this->rows = rows;
+	this->handler = &hi2c1;
+	this->bkl_config = LCD_BACKLIGHT;
+	this->lcd_control = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+
+	HAL_Delay(50);
+
+	this->backlight(false);
+	HAL_Delay(1000);
+
+	// we start in 8bit mode, try to set 4 bit mode
+	this->send(0x03 << 4, 0);
+	HAL_Delay(5); // wait min 4.1ms
+
+	// second try
+	this->send(0x03 << 4, 0);
+	HAL_Delay(5); // wait min 4.1ms
+
+	// third go!
+	this->send(0x03 << 4, 0);
+	HAL_Delay(2);
+
+	// finally, set to 4-bit interface
+	this->send(0x02 << 4, 0);
+
+	this->send(LCD_FUNCTIONSET | this->lcd_function, 0);
+
+	this->lcd_control |= LCD_DISPLAYON;
+	this->send(LCD_DISPLAYCONTROL | this->lcd_control, 0);
+
+	this->clear();
+
+	this->lcd_mode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+	this->send(LCD_ENTRYMODESET | this->lcd_mode, 0);
+
+	this->home();
+}
+
+void I2C_LCD::home() {
+	this->send(LCD_RETURNHOME, 0);
+	HAL_Delay(LCD_DELAY_MS);
+}
+
+void I2C_LCD::clear() {
+	this->send(LCD_CLEARDISPLAY, 0);
+	HAL_Delay(LCD_DELAY_MS);
+}
+
+void I2C_LCD::set_cursor(uint8_t x, uint8_t y) {
+	switch (y) {
+	case 0:
+		this->send(x | LCD_SETDDRAMADDR, 0);
+		HAL_Delay(1);
+		break;
+	case 1:
+		this->send((0x40 + x) | LCD_SETDDRAMADDR, 0);
+		HAL_Delay(1);
+		break;
+	case 2:
+		this->send((0x14 + x) | LCD_SETDDRAMADDR, 0);
+		HAL_Delay(1);
+		break;
+	case 3:
+		this->send((0x54 + x) | LCD_SETDDRAMADDR, 0);
+		HAL_Delay(1);
+		break;
+	default:
+		break;
+	}
+}
+
+void I2C_LCD::backlight(bool enable) {
+	if (enable) {
+		this->bkl_config = LCD_BACKLIGHT;
+	} else {
+		this->bkl_config = LCD_NOBACKLIGHT;
+	}
+
+	this->send(0, 0);
+}
+
+void I2C_LCD::on_off(bool on) {
+	if (on) {
+		this->lcd_control |= LCD_DISPLAYON;
+		this->send(LCD_DISPLAYCONTROL | this->lcd_control, 0);
+	} else {
+		this->lcd_control &= ~LCD_DISPLAYON;
+		this->send(LCD_DISPLAYCONTROL | this->lcd_control, 0);
+
+	}
+}
+
+void I2C_LCD::blink(bool on) {
+	if (on) {
+		this->lcd_control |= LCD_BLINKON;
+		this->send(LCD_DISPLAYCONTROL | this->lcd_control, 0);
+	} else {
+		this->lcd_control &= ~LCD_BLINKON;
+		this->send(LCD_DISPLAYCONTROL | this->lcd_control, 0);
+	}
+}
+
+void I2C_LCD::cursor(bool on) {
+	if (on) {
+		this->lcd_control |= LCD_CURSORON;
+		this->send(LCD_DISPLAYCONTROL | this->lcd_control, 0);
+	} else {
+		this->lcd_control &= ~LCD_CURSORON;
+		this->send(LCD_DISPLAYCONTROL | this->lcd_control, 0);
+	}
+}
+
+// This is for text that flows Left to Right
+void I2C_LCD::entry_mode(bool ltr) {
+	if (ltr) {
+		this->lcd_mode |= LCD_ENTRYLEFT;
+		this->send(LCD_ENTRYMODESET | this->lcd_mode, 0);
+	} else {
+		this->lcd_mode &= ~LCD_ENTRYLEFT;
+		this->send(LCD_ENTRYMODESET | this->lcd_mode, 0);
+	}
+}
+
+void I2C_LCD::send(uint8_t value, uint8_t mode) {
+	HAL_StatusTypeDef res;
+
+	// wait for device be ready
+	for (;;) {
+		res = HAL_I2C_IsDeviceReady(this->handler, this->address, 1,
+		HAL_MAX_DELAY);
+
+		if (res == HAL_OK) {
+			break;
+		}
+	}
+
+	uint8_t up = value & 0xf0;
+	uint8_t lo = (value << 4) & 0xf0;
+
+	uint8_t data_arr[4];
+	data_arr[0] = up | mode | this->bkl_config | PIN_EN;
+	data_arr[1] = up | mode | this->bkl_config;
+	data_arr[2] = lo | mode | this->bkl_config | PIN_EN;
+	data_arr[3] = lo | mode | this->bkl_config;
+
+	HAL_I2C_Master_Transmit(this->handler, this->address, data_arr,
+			sizeof(data_arr),
+			HAL_MAX_DELAY);
+
+	HAL_Delay(LCD_DELAY_MS);
+}
+
+void I2C_LCD::write_str(char *str) {
+	while (*str) {
+		this->send((uint8_t) (*str), 1);
+		str++;
+	}
+}
+
+#ifdef __cplusplus
+}
+#endif
+
---
+++ i2c_lcd.h
@@ -0,0 +1,94 @@
+#ifndef INC_I2C_LCD_H_
+#define INC_I2C_LCD_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "stm32f4xx_hal.h"
+#include "i2c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PIN_RS    (1 << 0)	// 0 command 1 data
+#define PIN_EN    (1 << 2)	// read
+#define LCD_DELAY_MS 5		// pause for display to draw char
+
+// commands
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR 0x40
+#define LCD_SETDDRAMADDR 0x80
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00
+
+// flags for function set
+#define LCD_4BITMODE 0x00
+
+#define LCD_5x10DOTS 0x04
+#define LCD_5x8DOTS 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+
+#define LCD_BACKLIGHT 0x08
+#define LCD_NOBACKLIGHT 0x0
+
+
+class I2C_LCD {
+private:
+	uint8_t cols;
+	uint8_t rows;
+	uint8_t address;
+	uint8_t lcd_mode;
+	uint8_t lcd_control;
+	uint8_t lcd_function;
+	uint8_t bkl_config;
+	I2C_HandleTypeDef *handler;
+	void send(uint8_t value, uint8_t mode);
+public:
+	I2C_LCD(uint8_t address, uint8_t cols, uint8_t rows);
+	void home();
+	void set_cursor(uint8_t col, uint8_t row);
+	void clear();
+	void backlight(bool enable);
+	void on_off(bool on);
+	void blink(bool on);
+	void cursor(bool on);
+	void entry_mode(bool ltr);
+	void write_str(char *str);
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_I2C_LCD_H_ */
